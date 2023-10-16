5d5bce83432879fa7190711a727f124e62f34e71
giansingh4710 <giansingh4710@gmail.com>
2023-10-16 16:46:30 +0000

    done

--- saveEmail.php
+++ saveEmail.php
@@ -1,23 +1,27 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 
-//ini_set('display_errors', 1);
-//error_reporting(E_ALL);
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
 
-echo "<pre>";
-var_dump($_POST);
-echo "</pre>";
+//echo "<pre>";
+//var_dump($_POST);
+//echo "</pre>";
 
-// $fp = fopen("./theText.txt", "w"); 
-// if ($fp === false) {
-//     die("Failed to open the file for writing.");
-// }
-//
-// //ob_flush();
-// if (fwrite($fp, $_POST["theText"]) === false) {
-//     die("Failed to write to the file.");
-// }
-// fclose($fp); 
-//
-// header("Location: ./index.html");
+$fp = fopen("./attendence.txt", "a"); 
+if ($fp === false) {
+    die("Failed to open the file for writing.");
+}
+
+date_default_timezone_set('US/Eastern');
+$writeTxt = date('M d Y H:i:s') . ", ";
+$writeTxt .= $_POST['email'] . "\n";
+
+if (fwrite($fp, $writeTxt) === false) {
+    die("Failed to write to the file.");
+}
+fclose($fp); 
+
+echo "Successful!!";
+//header("Location: ./index.html");
 ?>
