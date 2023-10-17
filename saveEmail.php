434d3fa6920ed6c77c0bb2779f2b60ba6cd16e7b
giansingh4710 <giansingh4710@gmail.com>
2023-10-17 21:03:08 +0000

    full name fixed

--- saveEmail.php
+++ saveEmail.php
@@ -4,9 +4,9 @@ header("Access-Control-Allow-Origin: *");
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-//echo "<pre>";
-//var_dump($_POST);
-//echo "</pre>";
+echo "<pre>";
+var_dump($_POST);
+echo "</pre>";
 
 $fp = fopen("./attendence.txt", "a"); 
 if ($fp === false) {
@@ -14,7 +14,8 @@ if ($fp === false) {
 }
 
 date_default_timezone_set('US/Eastern');
-$writeTxt = date('M d Y H:i:s') . ", $_POST['full_name'], $_POST['email']\n";
+$writeTxt = date('M d Y H:i:s') . ", ";
+$writeTxt.= $_POST['full_name'] . ", ". $_POST['email'] . "\n";
 if (fwrite($fp, $writeTxt) === false) {
     die("Failed to write to the file.");
 }
